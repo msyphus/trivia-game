3ec1e42eeb8ae5870322d09925199528ea72b8d4
msyphus <mark.syphus@gmail.com>
2020-04-01 22:08:58 -0600

    debugged when final answer is correct, clarified correct answer on explanation text, adjusted time allotment for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var questions = [
         quest: "What is the main reason companies fail when implementing changes?",
         answers: ["Lack of training", "Lack of resources (money, time, etc.)", "Poor employees", "Failure to create the right work culture"],
         correct: "Failure to create the right work culture",
-        explain: "It is common to blame circumstances or people when failure occurs, but failure to create an appropriate work culture is a root \
+        explain: "Correct! It is common to blame circumstances or people when failure occurs, but failure to create an appropriate work culture is a root \
                  problem that creates unfavorable circumstances.",
         source: "For more information:  \"How You Are Killing Your Lean Program and Don\'t Know it.\" Mark Syphus, 2019."
     },
@@ -11,7 +11,7 @@ var questions = [
         quest: "What is the most critical customer feedback to investigate?",
         answers: ["Complaints", "Compliments", "Saying everything is \"Just fine\"", "Silence"],
         correct: "Saying everything is \"Just fine\"",
-        explain: "If customers tell you this, they either don\'t trust that you'll do anything about their feedback or they don't care to give it to you. \
+        explain: "Correct! If customers tell you this, they either don\'t trust that you'll do anything about their feedback or they don't care to give it to you. \
                  Is it because they have already decided to go elsewhere? Is it because they are not loyal? You'll never know if you don't investigate!",
         source: "For more information: \"Raving Fans.\" Ken Blanchard and Sheldon Bowles, 1993."
     },
@@ -19,14 +19,14 @@ var questions = [
         quest: "To have a successful business, you should set a goal to have satisfied customers.",
         answers: ["True", "False"],
         correct: "False",
-        explain: "Having satisfied customers is a low target that won't create customer loyalty.  For long-term success, you need to create fans!",
+        explain: "Correct! Having satisfied customers is a low target that won't create customer loyalty.  For long-term success, you need to create fans!",
         source: "For more information: \"Raving Fans.\" Ken Blanchard and Sheldon Bowles, 1993."
     },
     {
         quest: "What is the first thing you should do to inspire people to excel in their work?",
         answers: ["Show the importance of what they do", "Give them autonomy to do their work and show you trust their judgment", "Give them timely, sincere praise", "Give them a financial incentive to hit a target"],
         correct: "Show the importance of what they do",
-        explain: "If people don't understand the importance of what they do, they will not be motivated enough to excel in difficult conditions.  The other \
+        explain: "Correct! If people don't understand the importance of what they do, they will not be motivated enough to excel in difficult conditions.  The other \
                  options are also important, but they have to be built on a foundation of understanding why the work being done is important.",
         source: "For more information: \"Gung Ho.\" Ken Blanchard and Sheldon Bowles, 1998."
     },
@@ -34,7 +34,7 @@ var questions = [
         quest: "You create a KPI for customer satisfaction based off of customer reviews, complaints, and surveys that you review each month.  Is this a good strategy?",
         answers: ["Yes", "No", "Depends on the business, market, or culture"],
         correct: "No",
-        explain: "Although this is a well-rounded approach for customer feedback, it does not consider leading indicators and the review interval is too long. \
+        explain: "Correct! Although this is a well-rounded approach for customer feedback, it does not consider leading indicators and the review interval is too long. \
                  By the time you know there's a problem, it's already too late!",
         source:  "For more information: \"When Leaders Torture Their Employees.\" Daniel Markovitz, 2020."
     },
@@ -42,14 +42,14 @@ var questions = [
         quest: "It is best to avoid having fun in the workplace so you can maintain a professional atmosphere.",
         answers: ["True", "False"],
         correct: "False",
-        explain: "Creating a fun environment is a great way to boost results as long as it is controlled.",
+        explain: "Correct! Creating a fun environment is a great way to boost results as long as it is controlled.",
         source: "For more information: \"FISH! A Proven Way to Boost Morale and Improve Results\" Stephen C. Lundin, Harry Paul, and John Christensen, 2000."
     },
     {
         quest: "A successful manager:",
         answers: ["Achieves results", "Builds good relationships with subordinates", "Strikes a balance between results and relationships"],
         correct: "Strikes a balance between results and relationships",
-        explain: "There are two kinds of bad managers: One who gets results but is hated by subordinates and one who is loved by subordinates but can't achieve good business results. \
+        explain: "Correct! There are two kinds of bad managers: One who gets results but is hated by subordinates and one who is loved by subordinates but can't achieve good business results. \
                  The challenge to being a good manager is to find the balance between the two.",
         source: "For more information: \"The New One Minute Manager.\" Ken Blanchard and Spencer Johnson, 2015."
     }
@@ -59,7 +59,7 @@ var randQuest;
 var chosenAns;
 var correct = 0;
 var incorrect = 0;
-var time = 12;
+var time = 30;
 var timeInterval;
 var timeRunning = false;
 var i = 0;
@@ -69,7 +69,8 @@ $(document).ready(function() {
     $("button").on("click", function() {
         randQuest = questions.sort(function (a, b) {
             return 0.5 - Math.random();
-        })      
+        });  
+        console.log(randQuest);    
         newQuestion();
         $(".startButton").hide();
     });
@@ -151,7 +152,11 @@ $(document).ready(function() {
             correct++;
             $("#explanation").text(randQuest[i].explain);
             $("#reference").text(randQuest[i].source);
-            setTimeout(newQuestion, 10000);
+            if( i < randQuest.length -1) {
+                setTimeout(newQuestion, 10000);
+            } else {
+                setTimeout(endQuiz, 10000);
+            }
         } else {
             incorrect++;
             $("#explanation").text("Sorry!  That's not the best answer.");
